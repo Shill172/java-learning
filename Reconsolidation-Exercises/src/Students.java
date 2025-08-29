41453b3a608bf2ff1077dc7e512e9c771f7b0003
Scott <scotthill006@gmail.com>
2025-08-29 20:30:01 +0100

    Added input validation to grade input

--- Reconsolidation-Exercises/src/Students.java
+++ Reconsolidation-Exercises/src/Students.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Students {
     ArrayList<String> names = new ArrayList<>();
-    HashMap<String, Character> grades = new HashMap<>();
+    HashMap<String, String> grades = new HashMap<>();
 
     // getNames asks the user to enter 5 names
     public ArrayList<String> getNames() {
@@ -24,20 +24,37 @@ public class Students {
 
     // getGrades asks the user to enter grades for existing students and stores them in a HashMap
     // If no Students are found in names ArrayList, getNames is called to populate it
-    public HashMap<String, Character> getGrades() {
+    public HashMap<String, String> getGrades() {
         Scanner sc = new Scanner(System.in);
         if (names.isEmpty()) {
             System.out.println("No students found, calling getNames");
             getNames();
-        } else {
-            for (String name : names) {
-                System.out.println("Please enter the grade for " + name + " (A, B, C, D, F)");
-                char grade = sc.nextLine().charAt(0);
-                grades.put(name, grade);
-            }
+        }
+        for (String name : names) {
+            System.out.println("Please enter the grade for " + name + " (A, B, C, D, F)");
+            String grade = sc.nextLine();
+            grade = validateGrade(grade);
+            grades.put(name, grade);
+
         }
         System.out.println(grades);
         return grades;
+    }
+
+    private String validateGrade(String grade) {
+        Scanner validateScanner = new Scanner(System.in);
+        boolean valid = false;
+        while (!valid) {
+            if (grade.equals("A" ) || grade.equals("B") || grade.equals("C") || grade.equals("D") || grade.equals("F")) {
+                System.out.println("Grade '" + grade + "' validated!");
+                valid = true;
+            } else {
+                System.out.println("Grade '" + grade + "' not validated! Please enter a valid grade: (A, B, C, D, F)");
+                grade = validateScanner.nextLine();
+            }
+
+        }
+        return grade;
     }
 
 
