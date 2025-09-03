11deb46e6ce6cf744aec32f4819b1020f58e7c1e
Scott <scotthill006@gmail.com>
2025-09-03 14:32:50 +0100

    Initial commit outlining goals for this project

---
+++ Reconsolidation-Exercises/Inheritance_Practice_Game/src/PlayGame.java
@@ -0,0 +1,13 @@
+// Building a small game to practice inheritance.
+// I will try to implement parent/subclasses, overriding, polymorphism, abstract classes, and interfaces
+
+public class PlayGame {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
